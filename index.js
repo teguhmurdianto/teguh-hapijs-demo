8781913f1fcf89eda2fb59ae9543a1eb236c0855
Teguh Murdianto <murdianto1561@gmail.com>
2018-04-20 11:49:47 +0700

    second commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('hapi');
+const server = new Hapi.Server({ port: 1337, host: '127.0.0.1'});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, h){
+        return 'Hello World!';
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function(request, h){
+        return 'Hello ' + encodeURIComponent(request.params.name) + '!';
+    }
+});
+
+const init = async() => {
+    await server.register(require('inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/welcome',
+        handler: function(request, h) {
+            return h.file('./public/welcome.html');
+        }
+    });
+
+    await server.start();
+    console.log(`server running at ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+})
+
+init();
